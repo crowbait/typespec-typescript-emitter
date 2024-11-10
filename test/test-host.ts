73f80bd31533cb70861eac467aa4b4d326dfa08d
Traxx <31518769+crowbait@users.noreply.github.com>
2024-11-10 20:34:14 +0100

    init

---
+++ test/test-host.ts
@@ -0,0 +1,47 @@
+import { Diagnostic, resolvePath } from "@typespec/compiler";
+import {
+  createTestHost,
+  createTestWrapper,
+  expectDiagnosticEmpty,
+} from "@typespec/compiler/testing";
+import { TypespecTypescriptRoutesTestLibrary } from "../src/testing/index.js";
+
+export async function createTypespecTypescriptRoutesTestHost() {
+  return createTestHost({
+    libraries: [TypespecTypescriptRoutesTestLibrary],
+  });
+}
+
+export async function createTypespecTypescriptRoutesTestRunner() {
+  const host = await createTypespecTypescriptRoutesTestHost();
+
+  return createTestWrapper(host, {
+    compilerOptions: {
+      noEmit: false,
+      emit: ["typespec-typescript-routes"],
+    },
+  });
+}
+
+export async function emitWithDiagnostics(
+  code: string
+): Promise<[Record<string, string>, readonly Diagnostic[]]> {
+  const runner = await createTypespecTypescriptRoutesTestRunner();
+  await runner.compileAndDiagnose(code, {
+    outputDir: "tsp-output",
+  });
+  const emitterOutputDir = "./tsp-output/typespec-typescript-routes";
+  const files = await runner.program.host.readDir(emitterOutputDir);
+
+  const result: Record<string, string> = {};
+  for (const file of files) {
+    result[file] = (await runner.program.host.readFile(resolvePath(emitterOutputDir, file))).text;
+  }
+  return [result, runner.program.diagnostics];
+}
+
+export async function emit(code: string): Promise<Record<string, string>> {
+  const [result, diagnostics] = await emitWithDiagnostics(code);
+  expectDiagnosticEmpty(diagnostics);
+  return result;
+}
